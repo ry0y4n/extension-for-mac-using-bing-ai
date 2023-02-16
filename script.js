975b42b47840e873338b9e1afa12d6a6a64fdfab
momosuke <rusk.ryoy4n@gmail.com>
2023-02-16 17:36:47 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+};
+
+function keyHandler(event) {
+    if (!(event.metaKey && event.key === 'Enter')) {
+        event.stopPropagation();
+    }
+}
+
+(async () => {
+    var input = null;
+    while (1) {
+        try {
+            input = document.getElementsByClassName('cib-serp-main')[0].shadowRoot.getElementById("cib-action-bar-main").shadowRoot.getElementById('searchbox');
+
+            if (input) {
+                console.log(input);
+                input.addEventListener("keydown", event => keyHandler(event), true);
+                input.addEventListener("keyup", event => keyHandler(event), true);
+            }
+            break;
+        }
+        catch (e) {
+            //console.log(e);
+            await sleep(500);
+
+        }
+    }
+})();
