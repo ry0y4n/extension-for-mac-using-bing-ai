3267ceb2f430cb1a656e64813665e63070a1aadf
momosuke <rusk.ryoy4n@gmail.com>
2023-04-28 08:51:39 +0900

    remove: console.log

--- script.js
+++ script.js
@@ -15,14 +15,12 @@ function keyHandler(event) {
             input = document.getElementsByClassName('cib-serp-main')[0].shadowRoot.getElementById("cib-action-bar-main").shadowRoot.getElementById('searchbox');
 
             if (input) {
-                console.log(input);
                 input.addEventListener("keydown", event => keyHandler(event), true);
                 input.addEventListener("keyup", event => keyHandler(event), true);
             }
             break;
         }
         catch (e) {
-            //console.log(e);
             await sleep(500);
 
         }
